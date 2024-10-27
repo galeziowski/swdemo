ce22f614171900c17b37547a76dc8ba463fcf397
Łukasz Gałęziowski <lukasz@motivlab.com>
2024-10-27 16:54:15 +0100

    test1

--- src/main/java/com/example/demo/owner/OwnerController.java
+++ src/main/java/com/example/demo/owner/OwnerController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -44,11 +45,11 @@ public class OwnerController {
     }
 
     @PostMapping
-    public void createOwnerProfile(OwnerProfile ownerProfile){
+    public void createOwnerProfile(@RequestBody OwnerProfile ownerProfile){
     }
 
     @PutMapping
-    public OwnerProfile updateOwnerProfile(OwnerProfile ownerProfile){
+    public OwnerProfile updateOwnerProfile(@RequestBody OwnerProfile ownerProfile){
         return ownerProfile;
     }
 
--- src/main/java/com/example/demo/vet/SampleController.java
+++ src/main/java/com/example/demo/vet/SampleController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,11 +19,11 @@ public class SampleController {
     }
 
     @PostMapping
-    public void createVetProfile(VetProfile vetProfile){
+    public void createVetProfile( @RequestBody VetProfile vetProfile){
     }
 
     @PutMapping
-    public VetProfile updateVetProfile(VetProfile vetProfile){
+    public VetProfile updateVetProfile(@RequestBody VetProfile vetProfile){
         return vetProfile;
     }
 
