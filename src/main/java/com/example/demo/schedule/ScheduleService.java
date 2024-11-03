6cbb67777f6e4b8b92301011b8d8128086ff29c9
Łukasz Gałęziowski <lukasz@motivlab.com>
2024-11-03 11:57:50 +0100

    Schedule skeleton added

--- src/main/java/com/example/demo/schedule/ScheduleService.java
+++ src/main/java/com/example/demo/schedule/ScheduleService.java
@@ -18,7 +18,7 @@ public class ScheduleService {
     private static final Schedule FIRST_SCHEDULE = new Schedule("1", LocalDate.of(2024, 11, 1),
             LocalDate.of(2024, 11, 30), 8, 16);
 
-    private static final Slot reserved = new Slot(LocalDateTime.of(2024, 11, 1, 9, 0), "1");
+    private static final List<Slot> reserved = List.of(new Slot(LocalDateTime.of(2024, 11, 1, 9, 0), "1"));
 
     public List<Slot> getSlots() {
         //List<Slot> slots = new ArrayList<>();
@@ -42,20 +42,7 @@ public class ScheduleService {
                 )
                 .collect(Collectors.toList());
 
-       /* for (int i = 0; i < days; i++) {
-            for (int hour = 0; hour < hoursForDay; hour++) {
-                int finalI = i;
-                int finalHour = hour;
-                if (!FIRST_SCHEDULE.startingDate.getDayOfWeek().plus(i).equals(DayOfWeek.SATURDAY) && !FIRST_SCHEDULE.startingDate.getDayOfWeek().plus(i).equals(DayOfWeek.SUNDAY)) {
-                    List.of(0, 30).forEach(countedMinute -> slots.add(new Slot(
-                            LocalDateTime.of(FIRST_SCHEDULE.startingDate.getYear(), FIRST_SCHEDULE.startingDate.getMonth(),
-                                    FIRST_SCHEDULE.startingDate.getDayOfMonth() + finalI, FIRST_SCHEDULE.startingHour + finalHour, countedMinute),
-                            "1")));
-                }
-            }
-        }*/
-
-        slots.removeAll(List.of(reserved));
+        slots.removeAll(reserved);
 
         return slots;
     }
